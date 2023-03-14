d03ce0bc9af146a2994db77bb166c54f172625f8
Streesss <streesfactor@gmail.com>
2023-03-14 08:17:02 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace rppg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private statsheet chara = new statsheet();
+        public MainWindow()
+        {
+            InitializeComponent();
+            statsheet stats = new statsheet();
+            //updatestats();
+            statsheet pal = new statsheet() { name = "hi" };
+            statsheet pal2 = new statsheet() { name = "hi 2" };
+            //chara.Partymembers.Add(pal);
+            //MessageBox.Show($"strenght: {stats.strength}");
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            chara.name = textBoxName.Text;
+            chara.roll();
+            // MessageBox.Show(chara.name);
+            foreach (ItemsControl item in partylist.Items)
+            {
+                statsheet r = new statsheet()
+                { name = item.Content.ToString() };
+
+                chara.PartyMembers.Add(r);
+            }
+            updatestats();
+            foreach(statsheet r in chara.Partymembers)
+            {
+                ListBoxItem
+            }
+        }
+
+        private void updatestats()
+        {
+            strengthnumber.Text = chara.strength.ToString();
+            charizzmanumber.Text = chara.charizzma.ToString();
+            intelligencenumber.Text = chara.intelligence.ToString();
+            wisdomnumber.Text = chara.wisdom.ToString();
+            lucknumber.Text = chara.luck.ToString();
+            dexeritynumber.Text = chara.dexerity.ToString();
+        }
+    }
+}
---
+++ statsheet.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rppg
+{
+    internal class statsheet// : INotifyPropertyChanged
+    {
+        #region fields
+        private CharacterClasses _characterClasses;
+
+        #region stats
+        private int _mindexerity = 1;
+        private int _dexerity;
+        private int _maxdexerity = 20;
+        private int _minintelligence = 1;
+        private int _intelligence;
+        private int _maxintelligence = 20;
+        private int _minstrength = 1;
+        private int _strength;
+        private int _maxstrength = 20;
+        private int _minwisdom = 1;
+        private int _wisdom;
+        private int _maxwisdom = 20;
+        private int _minluck = 1;
+        private int _luck;
+        private int _maxluck = 20;
+        private int _mincharizzma = 1;
+        private int _charizzma;
+        private int _maxcharizzma = 20;
+        #endregion
+        private List<statsheet> Partymembers = new List<statsheet>;
+        private CharacterClasses _characterClasses = Class.None;
+        private Random rng = new Random();
+        #endregion
+
+        #region properties
+        public CharacterClasses Class
+        {
+            get { return _characterClasses; }
+            set { _characterClasses = value; }
+        }
+        public string name { get; set; }
+        public long XP { get; set; }
+        public int level { get; set; }
+        /*public int CharacterClasses Class
+            {
+                
+            }*/
+
+        public int strength { get { return _strength; } }
+        public int dexerity { get { return _dexerity; } }
+        public int intelligence { get { return _intelligence; } }
+        public int wisdom { get { return _wisdom; } }
+        public int luck { get { return _luck; } }
+        public int charizzma { get { return _charizzma; } }
+        #endregion
+
+        public statsheet()
+        {
+            roll();
+        }
+
+        public void roll()
+        {
+            _charizzma = rng.Next(_mincharizzma, _maxcharizzma);
+            _intelligence = rng.Next(_minintelligence, _maxintelligence);
+            _strength = rng.Next(_minstrength, _maxstrength);
+            _wisdom = rng.Next(_minwisdom, _maxwisdom);
+            _luck = rng.Next(_minluck, _maxluck);
+            _dexerity = rng.Next(_mindexerity, _maxdexerity);
+        }
+
+        public static int rolldice (int numberofdice, int numberofsides)
+        {
+
+        }
+    }
+
+    public enum CharacterClasses
+    {
+        none = 0,
+        clreic = 1,
+        rouge = 2,
+        tank = 3,
+        mage = 4,
+        barbarian = 5,
+
+    }
+}
